5c77a6a77ed9853ed7b2a03a931f853cb829c577
Pavan Kumar <gopidesupavan@gmail.com>
2024-12-17 00:02:15 +0000

    test changes

--- artifacts/publish_packages_finder.py
+++ artifacts/publish_packages_finder.py
@@ -127,7 +127,7 @@ class PublishPackagesFinder:
     def run(self):
         try:
             console.print(f"[blue]{self.artifacts_config.get('description')}[/]")
-            console.print("\n")
+            console.print()
 
             self.final_packages_to_publish = self.exclude_packages_to_publish(
                 self.dev_svn_files, self.exclude_config
